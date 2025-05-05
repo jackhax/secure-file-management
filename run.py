5a8faea6537f1f6600a7f605d1f38b7134475eb4
Adnan Jakati <mj3184@nyu.edu>
2025-05-05 16:46:30 -0400

    workflow and ssl

--- run.py
+++ run.py
@@ -8,4 +8,9 @@ uploads_dir = os.path.join(app.root_path, '../uploads')
 os.makedirs(uploads_dir, exist_ok=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        debug=True,
+        ssl_context=('certs/cert.pem', 'certs/key.pem'),
+        host='0.0.0.0',
+        port=443
+    )
